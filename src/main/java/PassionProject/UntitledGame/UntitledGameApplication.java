d9eab76c3764d57ad31ffd8a7a78a701fa21a19e
Nathan <natefintak@gmail.com>
2025-04-25 09:44:07 -0400

    first commit

---
+++ src/main/java/PassionProject/UntitledGame/UntitledGameApplication.java
@@ -0,0 +1,13 @@
+package PassionProject.UntitledGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntitledGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UntitledGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PassionProject/UntitledGame/UntitledGameApplicationTests.java
@@ -0,0 +1,13 @@
+package PassionProject.UntitledGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UntitledGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
