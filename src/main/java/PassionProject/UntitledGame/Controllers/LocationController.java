2c1efac3484452e086457bab3a7c0b68d111d241
Nathan <natefintak@gmail.com>
2025-04-26 18:41:09 -0400

    still not displaying anything from the database

--- src/main/java/PassionProject/UntitledGame/Controllers/LocationController.java
+++ src/main/java/PassionProject/UntitledGame/Controllers/LocationController.java
@@ -7,14 +7,15 @@ import PassionProject.UntitledGame.Repos.LocationsRepo;
 import PassionProject.UntitledGame.Locations;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/locations")
 public class LocationController {
 
+    @Autowired
     public LocationsRepo locationsRepo;
 
-    @Autowired
     public LocationController(LocationsRepo locationsRepo) {
         this.locationsRepo = locationsRepo;
     }
@@ -26,27 +27,25 @@ public class LocationController {
     }
 
     @GetMapping("/list")
+    @PostMapping
     @ResponseBody
-    public String getLocations(Model template) {
-        Iterable<Locations> allLocations = this.getAllLocations();
-        template.addAttribute("location", allLocations);
-        return "locations_list";
+    public String getLocations(Model model) {
+        Iterable<Locations> allLocations = locationsRepo.findAll();
+        model.addAttribute("locations", allLocations);
+        return "location_list";
     }
 
     @GetMapping("/{id}")
-    @ResponseBody
-    public String getRoomName(@PathVariable Integer id, Model template) {
+    public String getLocation(@PathVariable(required = true) Integer id, Model model) {
         Locations location = locationsRepo.findById(id).orElseThrow(
                 () -> new RuntimeException("Room unknown")
         );
-        template.addAttribute("location", location);
-        return location.getLocationName() + "\n" + location.getInitDesc();
+        model.addAttribute("location", location);
+        return "location";
     }
 
 
     //getmapping connects to thymeleaf
-    // use /testlist for full list of test rooms
-    // putmapping is for editing
     //postmapping to post full list
     // pathvariable for getting primary key (id num)
     // requestbody for getting the formatting
